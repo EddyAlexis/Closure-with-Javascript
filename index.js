a5e542b2f89a8f04b4bc0e598c0675c1fac1f61b
EddyAlexis <134143077+EddyAlexis@users.noreply.github.com>
2023-10-15 21:24:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+//function greeting(greet){
+   // return function (name){
+    //    console.log(`${greet} ${name}`)
+ //   }
+//}
+
+//let greettingson = greeting(`hi`)
+//greettingson(`webster`)
+
+
+
+/*const countNumber = function(){
+    let count = 0;
+
+    return function (){
+        count++
+        console.log(`${count} number`)
+    }
+}
+
+let counter = countNumber();
+counter();
+counter();
+counter();
+counter();
+counter();*/
+
+
+var createFunction = function(){
+    var array = []
+
+    for(let i = 0;i < 3; i++){
+    array.push(() => (console.log(i)))
+    }
+
+    return array;
+}
+
+let arr =createFunction()
+arr[0]();
+arr[1]();
+arr[2]();
+
+
+/*var createFunction = function(){
+    var array = []
+
+    for(let i = 0;i < 3; i++){
+    array.push(function (){
+        console.log(i)
+    })
+    }
+
+    return array;
+}
+
+let arr =createFunction()
+arr[0]();
+arr[1]();
+arr[2]();*/
